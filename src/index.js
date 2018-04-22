66b89e1b3a38c87b965d36a856e7952a1f662884
Andrew <andrew.bestbier@gmail.com>
2018-04-22 19:20:29 +0100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './main.css';
-import './bulma.min.css';
-import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
-
-Main.embed(document.getElementById('root'));
-
-registerServiceWorker();
